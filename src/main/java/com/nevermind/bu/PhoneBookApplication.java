79e85b7be53659aa4d113c3064a9202c65bfcf1c
Roman <nevermind1993k@gmail.com>
2018-03-26 17:23:25 +0300

    Initial commit

---
+++ src/main/java/com/nevermind/bu/PhoneBookApplication.java
@@ -0,0 +1,13 @@
+package com.nevermind.bu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApplication {
+
+    public static void main(String[] args) {
+        // write your code here
+        SpringApplication.run(PhoneBookApplication.class, args);
+    }
+}
