998477460d75d0aca3de7f3b3d96ac36081c7d79
Roman <nevermind1993k@gmail.com>
2018-03-29 22:30:20 +0300

    Conditions for User fields added

--- src/main/java/com/nevermind/bu/entity/User.java
+++ src/main/java/com/nevermind/bu/entity/User.java
@@ -3,6 +3,9 @@ package com.nevermind.bu.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -13,19 +16,24 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column
+    @Pattern(regexp = "[a-zA-Z]+", message = "Username must have only English characters, " +
+            "without special characters")
+    @Size(min = 3, message = "Username size must be more then 3")
     private String userName;
 
-    @Column
+    @Size(min = 5, message = "Password size must be more then 5")
     private String password;
 
-    @Column
+    @Size(min = 5, message = "Name size must be more then 5")
+    @NotNull(message = "Name must be not null")
     private String name;
 
-    @Column
+    @Size(min = 5, message = "Last name size must be more then 5")
+    @NotNull(message = "Last name must be not null")
     private String lastName;
 
-    @Column
+    @Size(min = 5, message = "Middle name size must be more then 5")
+    @NotNull(message = "Middle name must be not null")
     private String middleName;
 
     public User() {
