d5b9294c94834f79bcf0f3eeac17c0cdd77b5f83
Roman <nevermind1993k@gmail.com>
2018-04-02 16:11:29 +0300

    Home Controller Test added

---
+++ src/test/java/com/nevermind/bu/controller/HomeControllerTest.java
@@ -0,0 +1,19 @@
+package com.nevermind.bu.controller;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class HomeControllerTest {
+
+    @Test
+    public void shouldHelloPage() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/")).andExpect(view().name("hello"));
+    }
+}
