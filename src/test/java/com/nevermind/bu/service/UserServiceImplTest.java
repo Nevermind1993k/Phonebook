c9c50eb02b92b88bbc0d4c2d1cfd20adf12a6b6f
Roman <nevermind1993k@gmail.com>
2018-04-02 15:54:15 +0300

    User Service Test added

--- src/test/java/com/nevermind/bu/service/UserServiceImplTest.java
+++ src/test/java/com/nevermind/bu/service/UserServiceImplTest.java
@@ -1,17 +1,76 @@
 package com.nevermind.bu.service;
 
-import org.junit.Assert;
+
+import com.nevermind.bu.entity.User;
+import com.nevermind.bu.service.interfaces.UserService;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class UserServiceImplTest extends InitMockService {
+import java.util.ArrayList;
 
-    @Test
-    public void getByUsernameTest() {
-        Assert.assertEquals(mockUser, userService.getByUsername("mockedUser"));
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class UserServiceImplTest {
+
+    @Mock
+    @Autowired
+    UserService userService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void getByIdTest() {
-        Assert.assertEquals(mockUser, userService.getById(1));
+    public void getByIdTest() throws Exception {
+        User mockedUser = new User();
+        mockedUser.setUsername("mocked");
+        mockedUser.setPassword("mocked");
+        mockedUser.setFirstName("mocked");
+        mockedUser.setLastName("mocked");
+        mockedUser.setMiddleName("mocked");
+        mockedUser.setContacts(new ArrayList<>());
+
+        when(userService.getById(anyInt())).thenReturn(mockedUser);
+        User byId = userService.getById(555);
+        assertEquals("mocked",byId.getUsername());
+
+        verify(userService).getById(555);
     }
+
+    @Test
+    public void getByUsernameTest() throws Exception {
+        User mockedUser = new User();
+        mockedUser.setUsername("mocked");
+        mockedUser.setPassword("mocked");
+        mockedUser.setFirstName("mocked");
+        mockedUser.setLastName("mocked");
+        mockedUser.setMiddleName("mocked");
+        mockedUser.setContacts(new ArrayList<>());
+
+        when(userService.getByUsername(anyString())).thenReturn(mockedUser);
+        User byUsername = userService.getByUsername("mocked");
+        assertEquals("mocked",byUsername.getUsername());
+    }
+
+    @Test
+    public void registerNewUserAccountTest() throws Exception {
+        User user = new User();
+        user.setUsername("New UserName");
+        userService.registerNewUserAccount(user);
+        verify(userService).registerNewUserAccount(user);
+    }
+
 }
