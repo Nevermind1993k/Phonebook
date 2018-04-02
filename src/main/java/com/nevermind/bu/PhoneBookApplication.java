852ba49ef9275b1ee150ed602f5a4f2f0df294eb
Roman <nevermind1993k@gmail.com>
2018-04-02 14:23:29 +0300

    Added load of properties to JVM by specifying path to properties

--- src/main/java/com/nevermind/bu/PhoneBookApplication.java
+++ src/main/java/com/nevermind/bu/PhoneBookApplication.java
@@ -3,11 +3,29 @@ package com.nevermind.bu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 @SpringBootApplication
 public class PhoneBookApplication {
 
     public static void main(String[] args) {
-        // write your code here
-        SpringApplication.run(PhoneBookApplication.class, args);
+//        SpringApplication.run(PhoneBookApplication.class, args);
+
+        SpringApplication application = new SpringApplication(PhoneBookApplication.class);
+        application.setAddCommandLineProperties(true);
+
+        String propertiesPath = "E:\\Dev\\New\\phonebook\\application.properties";
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream(new File(propertiesPath)));
+            application.setDefaultProperties(properties);
+            application.run(args);
+        } catch (IOException e) {
+            System.out.println("Can't find file by this path");
+            e.printStackTrace();
+        }
     }
 }
